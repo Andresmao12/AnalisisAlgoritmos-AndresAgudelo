563f9fdccfd48c9acf6cc9f851712ccce5e39fb6
mauro <andresmao205@gmail.com>
2026-09-15 06:23:00 -0500

    refactoriza: aplicamos PEP8 en calcular promedio

--- ejercicios-clase/semana-02/calcular-promedio.py
+++ ejercicios-clase/semana-02/calcular-promedio.py
@@ -1,8 +1,28 @@
-def CalcularPromedio(Lista):
-    s=0
-    for x in Lista:
-     s=s+x
-    return s/len(Lista)
- 
-l=[1,2,3,4,5]
-print(CalcularPromedio(l))
+def calcular_promedio(lista: list[int]) -> float:
+    """
+    Calcula el promedio de una lista de enteros
+
+    Args:
+        lista: Lista de enteros 
+    
+    Returns:
+        float: El promedio de los elementos en la lista
+    """
+
+    suma = 0
+
+    for elemento in lista:
+        suma += elemento
+    return suma / len(lista)
+
+
+
+def main() -> None:
+    lista = [1, 2, 3, 4, 5]
+    promedio = calcular_promedio(lista)
+
+    print(promedio)
+
+
+if __name__ == "__main__":
+    main()
